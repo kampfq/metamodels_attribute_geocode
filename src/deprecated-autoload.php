1c49c96c58c8d8461754944211a009442edb28e7
kampfq <ben@kampfq.eu>
2019-10-18 13:23:15 +0200

    fix issues

--- src/deprecated-autoload.php
+++ src/deprecated-autoload.php
@@ -1,21 +1,5 @@
 <?php
 
-/**
- * This file is part of KampfQ/attribute_geocode.
- *
- * (c) 2012-2019 The KampfQ team.
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- *
- * This project is provided in good faith and hope to be usable by anyone.
- *
- * @package    KampfQ/attribute_geocode
- * @author     David Molineus <david.molineus@netzmacht.de>
- * @copyright  2012-2019 The KampfQ team.
- * @license    https://github.com/KampfQ/attribute_geocode/blob/master/LICENSE LGPL-3.0-or-later
- * @filesource
- */
 
 use KampfQ\AttributeGeocodeBundle\Attribute\AttributeTypeFactory;
 use KampfQ\AttributeGeocodeBundle\Attribute\Geocode;
